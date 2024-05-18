47cd72b8a1d8ced4b070f02ff07c800ddf36e341
Emmanuel111 <emmanuel.montes@sofka.com.co>
2024-05-17 19:19:22 -0500

    Crate update get and delete clients is implement

--- src/main/java/com/company/financial/app/infrastructure/rest/controller/ClientController.java
+++ src/main/java/com/company/financial/app/infrastructure/rest/controller/ClientController.java
@@ -3,16 +3,10 @@ package com.company.financial.app.infrastructure.rest.controller;
 import com.company.financial.app.application.usecases.ClientService;
 import com.company.financial.app.domain.model.dto.request.ClientRequest;
 import com.company.financial.app.domain.model.responseRest.Response;
-import com.company.financial.app.infrastructure.rest.controller.ResExceptiont.DataClientException;
 import com.company.financial.app.infrastructure.rest.controller.validations.UserValidationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.zip.DataFormatException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,12 +19,25 @@ public class ClientController {
     private UserValidationData userValidationData;
 
     @PostMapping("/client")
-    public ResponseEntity<Response>createClient(@RequestBody ClientRequest clientRequest) {
+    public ResponseEntity<Response> createClient(@RequestBody ClientRequest clientRequest) {
         userValidationData.validateData(clientRequest);
         return clientService.createClient(clientRequest);
     }
 
+    @PutMapping("/client/update/{id}")
+    public ResponseEntity<Response> updateClient(@PathVariable Long clientId, @RequestBody ClientRequest clientRequest) {
+        userValidationData.validateData(clientRequest);
+        return clientService.updateClient(clientId, clientRequest);
+    }
 
+    @GetMapping("/client/{id}")
+    public ResponseEntity<Response> getClient(@PathVariable Long clientId){
+        return clientService.getClientById(clientId);
+    }
 
+    @PutMapping("/client/delete/{id}")
+    public ResponseEntity<Response> deleteClient(@PathVariable Long clientId) {
+        return clientService.deleteClientById(clientId);
+    }
 
 }
